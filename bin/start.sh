ae57d44bdaf7394dc62694b2887e031033b5c6d5
dingkexuan <dingkexuan0@gmail.com>
2024-04-16 19:01:44 +0800

    fix

--- bin/start.sh
+++ bin/start.sh
@@ -8,5 +8,5 @@ mkdir -p "$BASE_DIR/logs"
 echo $BASE_DIR
 cd $BASE_DIR
 sudo setcap cap_net_bind_service=+eip  $BASE_DIR/proxy_server
-nohup $BASE_DIR/proxy_server -backend_addr=localhost.sangfor.com.cn:8081 -proxy_addr=0.0.0.0:8082 >>  $BASE_DIR/logs/web_proxy.log 2>&1 &
+nohup $BASE_DIR/proxy_server -backend_addr=localhost:8081 -proxy_addr=0.0.0.0:8082 >>  $BASE_DIR/logs/web_proxy.log 2>&1 &
 ps -ef | grep "proxy_server" | grep -v "grep" | awk '{print $2}'
